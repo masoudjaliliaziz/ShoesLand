f12ae58a76963737688d32694f480142a2b78c0a
yousof <yosofasady2@gmail.com>
2025-01-09 12:38:12 +0330

    feat: payment design

--- ShoesLand/src/pages/Payment.tsx
+++ ShoesLand/src/pages/Payment.tsx
@@ -14,13 +14,14 @@ import paypal from "../assets/paypal-svgrepo-com.svg";
 import google from "../assets/google-color-svgrepo-com.svg";
 import apple from "../assets/apple-173-svgrepo-com.svg";
 import userpen from "../assets/user-pen-alt-svgrepo-com.svg";
+import MasterCard from "../assets/MasterCard.svg";
 
 const paymentMethods = [
   { id: 1, name: "My Wallet", icon: wallet },
   { id: 2, name: "PayPal", icon: paypal },
   { id: 3, name: "Google Pay", icon: google },
   { id: 4, name: "Apple Pay", icon: apple },
-  { id: 5, name: ".... .... .... 4679", icon: userpen },
+  { id: 5, name: ".... .... .... 4679", icon: MasterCard },
 ];
 
 const Payment: React.FC = () => {
@@ -71,17 +72,23 @@ const Payment: React.FC = () => {
       <div className=" space-y-3">
         {paymentMethods.map((method) => (
           <div
-            className="bg-white flex flex-row justify-between items-center px-4 py-3 rounded-lg "
+            className="bg-white flex flex-row justify-between items-center px-4 py-7 rounded-lg "
             key={method.id}
             onClick={() => handlePaymentMethodSelect(method.name)}
           >
-            <div className="flex felx-row space-x-2">
-              <img className="w-5" src={method.icon} />
-              <span className="font-semibold text-sm">{method.name}</span>
+            <div className="flex felx-row space-x-2 justify-center items-center">
+              <img className="w-8" src={method.icon} />
+              <span className="font-semibold text-sm ">{method.name}</span>
             </div>
             <div className="flex flex-row items-center space-x-2">
-              <span className="font-semibold text-sm"> {finalTotal}</span>
-              <input type="radio" name="" id="" className="accent-slate-950" />
+
+              {selectedPaymentMethod == method.name &&
+                <span className="font-semibold text-md text-gray-700"> ${finalTotal}</span>
+              }
+              <input type="radio" name="method" id="method"
+                className="w-5 h-5 accent-black"
+                checked={selectedPaymentMethod == method.name}
+              />
             </div>
           </div>
         ))}
@@ -92,14 +99,12 @@ const Payment: React.FC = () => {
           <h3>You have selected: {selectedPaymentMethod}</h3>
         </div>
       )} */}
-      <button onClick={handleCreateOrder}>
+      <button onClick={handleCreateOrder} >
         <div className=" fixed bottom-4 w-[420px] mx-auto">
-          <div className="h-1/2 bg-slate-950 font-semibold text-base py-2 text-center text-white w-[85%] rounded-3xl">
-            {selectedPaymentMethod && (
-              <span className="font-semibold text-sm text-white py-2">
-                Confrim Payment
-              </span>
-            )}
+          <div className="h-1/2 bg-black font-semibold text-base py-4 text-center text-white w-[85%] rounded-full">
+            <span className="font-semibold text-sm text-white py-5">
+              Confrim Payment
+            </span>
           </div>
         </div>
       </button>
